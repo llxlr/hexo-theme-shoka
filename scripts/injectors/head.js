d484843f2a178ec8760f80736a787ad8d81280a1
llxlr <i@xhlr.top>
2022-08-27 21:24:17 +0800

    🐛 更新注入器配置

--- scripts/injectors/head.js
+++ scripts/injectors/head.js
@@ -1,19 +1,20 @@
-'use strict';
+/* global hexo */
 
+'use strict';
 const css = hexo.extend.helper.get('css').bind(hexo);
 const js = hexo.extend.helper.get('js').bind(hexo);
 
-
 hexo.extend.injector.register('head_end', () => {
   const config = hexo.config;
-  const theme = hexo.theme.config;
+  const theme = hexo.config.theme_config;
 
   var vendors = [];
-
+  
   var busuanzi = theme.vendors.js.busuanzi;
-  if(busuanzi){
-    vendors.push({ async: true, src: busuanzi });
+  if(!busuanzi){
+    busuanzi = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js';
   }
+  vendors.push({ async: true, src: busuanzi });
 
   return js(vendors);
 })
