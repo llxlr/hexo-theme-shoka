25a192727caefa889efa76ec64b9be61322e41d0
llxlr <i@xhlr.top>
2026-06-14 15:38:43 +0800

    ✨ 添加 Meting API 服务器地址配置
    Signed-off-by: llxlr <i@xhlr.top>

--- scripts/generaters/script.js
+++ scripts/generaters/script.js
@@ -68,6 +68,7 @@ hexo.extend.generator.register('script', function(locals){
   if(theme.audio) {
     siteConfig.audio = theme.audio
   }
+  siteConfig.meting_api = theme.meting_api || 'https://api.i-meto.com/meting/api';
 
   var text = '';
 
